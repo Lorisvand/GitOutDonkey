9d88ab6296664cd66b454a78e419cf9c20529854
“Loris” <loris.van.damme@student.howest.be>
2024-11-04 10:54:27 +0100

    added room4

---
+++ js/room4.js
@@ -0,0 +1,24 @@
+let clickCount = 0;
+
+const image = document.getElementById('clickable-image');
+const number = document.getElementById('reveal-number');
+
+const init = () => {
+    image.addEventListener('click', function() {
+        clickCount++;
+        if (clickCount < 4) {
+          const maxX = window.innerWidth - image.width;
+          const maxY = window.innerHeight - image.height;
+          const randomX = Math.floor(Math.random() * maxX);
+          const randomY = Math.floor(Math.random() * maxY);
+          image.style.position = 'absolute';
+          image.style.left = randomX + 'px';
+          image.style.top = randomY + 'px';
+        } else {
+          image.style.display = 'none';
+          number.style.display = 'block';
+        }
+      });
+}
+
+init();
